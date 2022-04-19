bee096e125ba6d7836af983072b1a46e4c17cdb3
tofukuoka <tomoyuki.fukuoka@exmotion.co.jp>
2022-04-19 20:16:00 +0900

    Modify for WSL2

---
+++ ros/launch-ros2-amd-env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ROS_DISTRO=foxy
+if [ $# -eq 1 ]; then
+    ROS_DISTRO=$1
+fi
+
+# DockerコンテナからのX11のアクセスを許可
+UNAME_R=$(uname -r)
+if [[ ! "$UNAME_R" =~ WSL2 ]]; then
+    xhost + local:docker
+fi
+
+# コンテナの起動
+docker run  --rm --network host \
+ --name foxy \
+ -v /dev/shm:/dev/shm \
+ -v /tmp/.X11-unix:/tmp/.X11-unix \
+ -e DISPLAY \
+ -e XAUTHORITY \
+ -v=$(pwd):/workspace \
+ -w /workspace \
+ -it \
+ ros2-${ROS_DISTRO}-env 
+
+# DockerコンテナからのX11のアクセスを削除
+if [[ ! "$UNAME_R" =~ WSL2 ]]; then
+    xhost + local:docker
+fi
